8573ae5e2b33b04b19d6354741ee06d8a09b4a09
johankitti <johan.kitti@gmail.com>
2015-02-28 16:42:06 -0600

    asd

--- app.js
+++ app.js
@@ -56,8 +56,8 @@ app.post('/api/post-it/', function(req, res) {
 	}
 	postItRef.push(newPostIt)
 	io.emit('NewPostItCreated', newPostIt)
-	// res.status(200)
-	// res.send()
+	res.status(200)
+	res.send()
 })
 
 var server = app.listen(process.env.PORT || 8080, function () {
