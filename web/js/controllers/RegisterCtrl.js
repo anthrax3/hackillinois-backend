f23a334888f0b1b908f3de20de1dd5827308dd46
johankitti <johan.kitti@gmail.com>
2015-03-01 05:05:23 -0600

    tjena

--- web/js/controllers/RegisterCtrl.js
+++ web/js/controllers/RegisterCtrl.js
@@ -2,27 +2,27 @@
 /*global homeDashboard*/
 
 (function() {
-  'use strict';
-  var RegisterCtrl = function() {
- 		this.ref = new Firebase("https://amber-heat-5574.firebaseio.com");
-  };
+	'use strict';
+	var RegisterCtrl = function() {
+		this.ref = new Firebase("https://amber-heat-5574.firebaseio.com");
+	};
 
-  RegisterCtrl.prototype.register = function() {
-   this.ref.createUser({
-      email    : this.email,
-      password : this.password
-    }, function(error, userData) {
-      if (error) {
-        console.log("Error creating user:", error);
-      } else {
-        console.log("Successfully created user account with uid:", userData.uid);
-        var uref = this.ref.child('users/' + userData.uid);
-        uref.set({
-          name: 'putte',
-          email: this.email
-        })
-      }
-    });
-  };
-  hackIllinois.controller('RegisterCtrl', RegisterCtrl);
+	RegisterCtrl.prototype.register = function() {
+		this.ref.createUser({
+			email    : this.email,
+			password : this.password
+		}, function(error, userData) {
+		if (error) {
+			console.log("Error creating user:", error);
+		} else {
+			console.log("Successfully created user account with uid:", userData.uid);
+			var uref = this.ref.child('users/' + userData.uid);
+			uref.set({
+				name: 'putte',
+				email: this.email
+			})
+		}
+		}.bind(this));
+	};
+	hackIllinois.controller('RegisterCtrl', RegisterCtrl);
 }());
